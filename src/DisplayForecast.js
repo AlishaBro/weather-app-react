5c88f1b00a85584ebf9394c5510ea0b272daef33
Alisha Brodin <80694305+AlishaBro@users.noreply.github.com>
2026-07-10 11:15:34 +0200

    Fix forecast: replace deprecated onecall endpoint, move API keys to env vars

--- src/DisplayForecast.js
+++ src/DisplayForecast.js
@@ -1,45 +1,67 @@
-import React, {useState,useEffect } from "react";
-import './DisplayForecast.css'
+import React, { useState, useEffect } from "react";
+import "./DisplayForecast.css";
 
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
 
 export default function DisplayForecast(props) {
-    const [loaded, setLoaded] = useState(false)
-    const [forecast, setForecast] = useState();
-    function handleResponse(response) {
-        setForecast(response.data.daily)
-        setLoaded(true);
-    }
-      useEffect(() => {
-        setLoaded(false);
-      }, [props.coordinates]);
-    
-    
-  
-    if (loaded) {
-        return (
-            <div className="DisplayForecast">
-                <div className="row">
-                   
-                    {forecast.map(function (forecastDaily, index) {                  
-                        if (index < 5) {
-                            return (
-                                <div className="col" key={index}>
-                                    <ForecastDay data={forecastDaily} />
-                                </div>
-                            )
-                        } else { 
-                            return null;
-                        }
-                    })}    
-                </div>
-            </div>
-        )
-    } else { 
-        let apiurl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&appid=53f3bc1f5d348c44be3e3754c7185573&units=metric`
-        axios.get(apiurl).then(handleResponse);
-        return null;
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState();
 
-    }
+  function groupByDay(list) {
+    const days = {};
+    list.forEach((entry) => {
+      const date = new Date(entry.dt * 1000).toISOString().split("T")[0];
+      if (!days[date]) {
+        days[date] = {
+          dt: entry.dt,
+          weather: entry.weather,
+          temps: [entry.main.temp],
+        };
+      } else {
+        days[date].temps.push(entry.main.temp);
+        if (new Date(entry.dt * 1000).getHours() === 12) {
+          days[date].weather = entry.weather;
+        }
+      }
+    });
+    return Object.values(days).map((day) => ({
+      dt: day.dt,
+      weather: day.weather,
+      temp: { max: Math.max(...day.temps), min: Math.min(...day.temps) },
+    }));
+  }
+
+  function handleResponse(response) {
+    setForecast(groupByDay(response.data.list));
+    setLoaded(true);
+  }
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  if (loaded) {
+    return (
+      <div className="DisplayForecast">
+        <div className="row">
+          {forecast.map(function (forecastDaily, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <ForecastDay data={forecastDaily} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = process.env.REACT_APP_WEATHER_KEY;
+    let apiurl = `https://api.openweathermap.org/data/2.5/forecast?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiurl).then(handleResponse);
+    return null;
+  }
 }
--- src/Search.js
+++ src/Search.js
@@ -4,82 +4,75 @@ import DisplayForecast from "./DisplayForecast";
 import axios from "axios";
 import "./App.css";
 
-
-
-
 export default function Search() {
+  const [weatherData, setweatherData] = useState({ ready: false });
+  let [city, setCity] = useState(null);
+  // let [message, setMessage] = useState(null);
 
-    const [weatherData, setweatherData] = useState({ ready: false });
-    let [city, setCity] = useState(null);
-    // let [message, setMessage] = useState(null);
+  function handleUpdate(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
 
-  
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (city) {
+      let apiKey = process.env.REACT_APP_WEATHER_KEY;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      console.log("calling api");
 
-    function handleUpdate(event) {
-        event.preventDefault();
-        setCity(event.target.value)
-
+      axios.get(url).then(handlResponse);
     }
+  }
 
+  function handlResponse(response) {
+    console.log("here");
+    console.log(response);
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        if (city) {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5354b60afda2b7800186c06153932396&units=metric`;
-            console.log("calling api")
-            
-            axios.get(url).then(handlResponse)
-            
-        }
-    }
+    setweatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      feelsLike: response.data.main.feels_like,
+      description: response.data.weather[0].description,
+      place: response.data.name,
+    });
+  }
 
-    function handlResponse(response) {
-        console.log("here")
-        console.log(response)
+  if (weatherData.ready) {
+    return (
+      <div className="Search">
+        <form className="row" onSubmit={handleSubmit}>
+          <div className="col-9">
+            <input
+              type="text"
+              placeholder="Enter the city"
+              autoFocus="on"
+              required="on"
+              className="form-control"
+              onChange={handleUpdate}></input>
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn search-btn w-100"></input>
+          </div>
+        </form>
 
-      
-            setweatherData({
-                ready: true,
-                coordinates: response.data.coord,
-                temperature: response.data.main.temp,
-                date: new Date(response.data.dt * 1000),
-                icon: response.data.weather[0].icon,
-                humidity: response.data.main.humidity,
-                wind: response.data.wind.speed,
-                feelsLike: response.data.main.feels_like,
-                description: response.data.weather[0].description,
-                place: response.data.name,
-            })
-          
-    }
-   
-    if (weatherData.ready) {
-        return (
-            <div className="Search">
-            
-                <form className="row" onSubmit={handleSubmit}>
-                    <div className="col-9">
-                        <input type="text" placeholder="Enter the city" autoFocus="on" required="on" className="form-control" onChange={handleUpdate}></input>
-                    </div>
-                    <div className="col-3">
-                
-                        <input type="submit" value="search" className="btn search-btn w-100"></input>
-                    </div>
-                </form>
-
-                <DisplayWeather data={weatherData} />
-                <DisplayForecast coordinates={weatherData.coordinates} />
-           
-            </div>
-        )
-    }
-    
-        else {
-            let city = "New york";
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5354b60afda2b7800186c06153932396&units=metric`;
-            axios.get(url).then(handlResponse);
-            return "Loading...";
-        }
+        <DisplayWeather data={weatherData} />
+        <DisplayForecast coordinates={weatherData.coordinates} />
+      </div>
+    );
+  } else {
+    let city = "New york";
+    let apiKey = process.env.REACT_APP_WEATHER_KEY;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(handlResponse);
+    return "Loading...";
+  }
 }
-    
-
