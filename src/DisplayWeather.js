72578b47ea7610c641d5c217b3d8730af36990e7
Alisha Brodin <80694305+AlishaBro@users.noreply.github.com>
2026-07-14 10:34:06 +0200

    added css changes to correct the allignment of the weather display parameters

--- src/DisplayWeather.js
+++ src/DisplayWeather.js
@@ -1,49 +1,39 @@
 import React from "react";
+import "./DisplayWeather.css";
 import FormatDate from "./FormatDate";
-import AnimatedWeatherIcon from "./AnimatedWeatherIcon"
+import AnimatedWeatherIcon from "./AnimatedWeatherIcon";
 import UnitConversion from "./UnitConversion";
 
-export default function DisplayWeather(props) { 
-    return (
+export default function DisplayWeather(props) {
+  return (
     <div className="DisplayWeather">
-        <h1>{props.data.place}</h1>
-                 <div className="row ">
-                     <div className="col-4 d-flex float-left">
-                        <div className="canvas">
-                        {/* <img src={props.data.icon} alt="cloudy" /> */}
-                        <AnimatedWeatherIcon icon={props.data.icon} size={45} />
-                      </div>
-                      <div>
-                        <UnitConversion temperature={props.data.temperature} />
-                      </div>
-                  </div>
-                  <div className="col-4">
-                      <ul>
-                          <li>
-                              Feels like: {Math.round(props.data.feelsLike)}°C
-                          </li>
-                          <li>
-                              Humidity: {props.data.humidity}%
-                          </li>
-                          <li>
-                              Wind: {Math.round(props.data.wind)}Km/hr
-                          </li>
-                      </ul>
-    
-                  </div>
-                  <div className="col-4">
-                      <ul>
-                          <li>
-                            <FormatDate data={props.data.date} />
-                          </li>
-                          <li className="text-capitalize"> 
-                              {props.data.description}
-                          </li>
-                      </ul>
-                
-                  </div>
-                
-              </div>
-        </div> 
-    )
+      <h1>{props.data.place}</h1>
+      <div className="row ">
+        <div className="col-4 d-flex float-left">
+          <div className="canvas">
+            {/* <img src={props.data.icon} alt="cloudy" /> */}
+            <AnimatedWeatherIcon icon={props.data.icon} size={45} />
+          </div>
+          <div>
+            <UnitConversion temperature={props.data.temperature} />
+          </div>
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>Feels like: {Math.round(props.data.feelsLike)}°C</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)}Km/hr</li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <ul>
+            <li>
+              <FormatDate data={props.data.date} />
+            </li>
+            <li className="text-capitalize">{props.data.description}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
